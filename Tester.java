fc45fa6196792bf61f429615f74b70fe4c58ca22
Julia Zhu <zhujul24@7hills.org>
2024-02-03 22:02:50 -0500

    nothing is working rn lmao

--- Tester.java
+++ Tester.java
@@ -1,22 +1,23 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Tester {
   public static void main(String[] args) {
-  static String[][] starr = new String[100][20]; 
-  ArrayList<Student> students = new ArrayList<>();
-  ArrayList<Session> sessions = new ArrayList<Session>();
+    String[][] starr = new String[100][20]; 
+    ArrayList<Student> students = new ArrayList<>();
+    ArrayList<Session> sessions = new ArrayList<Session>();
+    ArrayList<Random> random = new ArrayList<Random>(); //used for the people who didn't sign up
 
-  int
-
-  String name;
-  int id;
-  int choice1;
-  int choice2;
-  int choice3;
-  int choice4;
-  int choice5;
+    String name;
+    int id;
+    int choice1;
+    int choice2;
+    int choice3;
+    int choice4;
+    int choice5;
   //fix this
   
 
@@ -24,15 +25,16 @@ public class Tester {
     File myObj = new File("SrSeminar_RawData.csv");
     Scanner myReader = new Scanner(myObj);
     while (myReader.hasNextLine()) {
-      String data = myReader.nextLine().split(",");
+      String[] data = myReader.nextLine().split(",");
+      System.out.print(starr[0][1]);
+      
+        /*choice1 = Integer.parseInt(choice[1]);
+        choice2 = Integer.parseInt(choice[2]);
+        choice3 = Integer.parseInt(choice[3]);
+        choice4 = Integer.parseInt(choice[4]);
+        choice5 = Integer.parseInt(choice[5]);
 
-        choice1 = Integer.parseInt();
-        choice1 = Integer.parseInt()
-        choice1 = Integer.parseInt()
-        choice1 = Integer.parseInt()
-        choice1 = Integer.parseInt()
-
-      row++;  
+     row++; 
 
       } //close while
       /*if (myObj.createNewFile()) {
@@ -42,16 +44,21 @@ public class Tester {
       }*/
    }
    
-   if(choice1 > 0){
+   /*if(choice1 > 0){
     students.add(new Student(name, choice1, choice2, choice3, choice4, choice5));
    }
-   else 
-   
+   else {
+    random.add(new Student(name, choice1, choice2, choice3, choice4, choice5));
+   }
+ */
+     
+  }
    
    catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
    }
 
-     
+   //System.out.print(name, choice1, choice2, choice3, choice4, choice5);
+}
 }
